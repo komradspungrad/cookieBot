158375a27c810ada88cacff1bfe859d4886934b7
komradspungrad <spangrpl@gmail.com>
2016-06-20 22:59:06 -0400

    changes
    -now with 100% more changes

--- autoBuy.js
+++ autoBuy.js
@@ -1,25 +1,52 @@
+//Create emptpy interval vars
+var intervalBuildings = null;
+var intervalUpgrades = null;
+var intervalClot = null;
+var intervalSeasons = null;
+var intervalSpecial = null;
+var intervalPrestige = null;
+
+//Common interface statics
+var toggle = document.getElementById("toggle");
+var tech = document.getElementById("techUpgrades");
+var upgrades = document.getElementById("upgrades");
+var store = document.querySelector("[class='storeSection']");
+var storeSell = document.querySelector("[class='storeSection selling']");
+var options = document.getElementById("prefsButton");
+var stats = document.getElementById("statsButton");
+var legacy = document.getElementById("legacyButton");
+
+
+//Interval manager
 function intervalSet(on) {
     if (on) {
-        var oneMs = setInterval(function () {
+        intervalBuildings = setInterval(function () {
             buyBuilding();
         }, 1);
-
-        var oneS = setInterval(function () {
+        intervalUpgrades = setInterval(function () {
             buyUpgrades();
+        }, 1000);
+        intervalClot = setInterval(function () {
             reloadForBadCookie();
-            specialUpgrade();
+        }, 1000);
+        intervalSeasons = setInterval(function () {
             seasons();
         }, 1000);
+        intervalSpecial = setInterval(function () {
+            specialUpgrade();
+        }, 1000);
+        intervalPrestige = setInterval(function () {
+            prestigeWorldWide();
+        }, 20000);
     }
     else {
-        clearInterval(oneMs);
-        clearInterval(oneS);
+        clearInterval(intervalBuildings);
+        clearInterval(intervalUpgrades);
+        clearInterval(intervalClot);
+        clearInterval(intervalSeasons);
+        clearInterval(intervalSpecial);
     }
 }
-
-var prestigeMe = setInterval(function () {
-    prestigeWorldWide();
-}, 20000);
 
 intervalSet(true);
 
@@ -30,10 +57,10 @@ function seasons() {
     var easter = Game.Has('Chicken egg') && Game.Has('Duck egg') && Game.Has('Turkey egg') && Game.Has('Quail egg') && Game.Has('Robin egg') && Game.Has('Ostrich egg') && Game.Has('Cassowary egg') && Game.Has('Salmon roe') && Game.Has('Frogspawn') && Game.Has('Shark egg') && Game.Has('Turtle egg') && Game.Has('Ant larva') && Game.Has('Golden goose egg') && Game.Has('Faberge egg') && Game.Has('Wrinklerspawn') && Game.Has('Cookie egg') && Game.Has('Omelette') && Game.Has('Century egg') && Game.Has('"egg"') && Game.UpgradesById[227] != null;
     var goldenCookieReload = Game.Has('Lucky day') && Game.Has('Serendipity') && Game.Has('Get lucky');
     var wrinklers = 0;
-    var valentineButton = document.querySelector("[onclick='Game.UpgradesById[184].buy();']");
-    var halloweenButton = document.querySelector("[onclick='Game.UpgradesById[183].buy();']");
-    var christmasButton = document.querySelector("[onclick='Game.UpgradesById[182].buy();']");
-    var easterButton = document.querySelector("[onclick='Game.UpgradesById[209].buy();']");
+    var valentineButton = toggle.querySelector("[onclick='Game.UpgradesById[184].buy();']");
+    var halloweenButton = toggle.querySelector("[onclick='Game.UpgradesById[183].buy();']");
+    var christmasButton = toggle.querySelector("[onclick='Game.UpgradesById[182].buy();']");
+    var easterButton = toggle.querySelector("[onclick='Game.UpgradesById[209].buy();']");
     for (i = 0; i < 12; i++) {
         if (Game.wrinklers[i].close == 1) {
             wrinklers++;
@@ -96,10 +123,9 @@ function specialUpgrade() {
 }
 
 function reload() {
-    var buttonOptions = document.getElementById("prefsButton");
     var optionsMenu = document.getElementsByClassName("option");
     var lenMenu = optionsMenu.length;
-    buttonOptions.click();
+    options.click();
     Game.WriteSave();
     Game.ExportSave();
     var saveData = document.getElementById("textareaPrompt").innerHTML;
@@ -125,7 +151,6 @@ function buyBuilding() {
     if (buy1.getAttribute("class") != "storeBulkAmount selected") {
         buy1.click();
     }
-    var store = document.querySelector("[class='storeSection']");
     var unlocked = store.getElementsByClassName("product unlocked enabled");
     var len = unlocked.length;
     for (var i = 0; i < len; i++) {
@@ -140,8 +165,7 @@ function sellBuildings() {
     Game.dragonAura = 5;
     document.getElementById("storeBulkSell").click();
     document.getElementById("storeBulkMax").click();
-    var store = document.querySelector("[class='storeSection selling']");
-    var unlocked = store.getElementsByClassName("product unlocked enabled");
+    var unlocked = storeSell.getElementsByClassName("product unlocked enabled");
     var len = unlocked.length;
     for (var i = 0; i < len - 1; i++) {
         unlocked[i].click();
@@ -175,44 +199,26 @@ function prestigeWorldWide() {
     }
 }
 
-var noPurchase = [71, 72, 73, 74, 79, 83, 84, 85, 87, 91, 124, 141, 142, 167, 181, 182, 183, 184, 185, 186, 189, 208, 209, 225, 227, 253, 254, 255, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 320, 321, 322, 323, 325, 326, 327, 328, 329, 331, 332, 333, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 368, 393, 394];
-var yesPurchase = ["empty"];
-var upgradeLen = Game.UpgradesById.length;
+//Ids for upgrade skips
+var noPurchase = [71, 72, 73]
 var skipLen = noPurchase.length;
-for (var i = 0; i < upgradeLen; i++) {
-    for (var j = 0; j < skipLen; j++) {
-        if (i == noPurchase[j].toString()) {
-            break;
-        }
-        else {
-            if (j === skipLen - 1) {
-                yesPurchase.push(i);
-            }
-        }
-    }
-}
 
+//Buy upgrades when they're available
 function buyUpgrades() {
-    if (yesPurchase.length > 1) {
-        for (var i = 0; i < yesPurchase.length; i++) {
-            var upgradeCheck = yesPurchase[i].toString();
-            var element = "[onclick='Game.UpgradesById[" + upgradeCheck + "].buy();']";
-            var upgradeClick = document.querySelector(element);
-            if (upgradeCheck === "69") {
-                if (upgradeClick != null) {
-                    if (upgradeClick.getAttribute("class") == "crate upgrade enabled") {
-                        upgradeClick.click();
-                        document.getElementById("promptOption0").click();
-                    }
-                }
+    var upgradesList = upgrades.getElementsByClassName("crate upgrade enabled");
+    var techList = tech.getElementsByClassName("crate upgrade enabled");
+    var upgradesLen = upgradesList.length;
+    var techLen = techList.length;
+    if (upgradesLen > 0) {
+        if ((upgradesLen === 1 && upgradesList[0].getAttribute("onclick") == "Game.UpgradesById[227].buy();") == false) {
+            for (i = 0; i < upgradesLen; i++) {
+                upgradesList[i].click();
             }
-            else {
-                if (upgradeClick != null) {
-                    if (upgradeClick.getAttribute("class") == "crate upgrade enabled") {
-                        upgradeClick.click();
-                    }
-                }
-            }
+        }
+    }
+    for (i = 0; i < skipLen; i++) {
+        if ((techList[0].getAttribute("onclick") == "Game.UpgradesById[" + noPurchase[i] + "].buy();") == false) {
+            techList[0].click();
         }
     }
 }
@@ -221,15 +227,7 @@ function buyUpgrades() {
 
 
 
-
-//cookieMonster
-
-function cookieMonster()
-{
-    Game.LoadMod('http://aktanusa.github.io/CookieMonster/CookieMonster.js');
-}
-
-//cookieBot
+/*
 
 function seasonPopup()
 {
@@ -265,46 +263,4 @@ var autoClicker = function (clicksAtOnce, repeatInterval)
 };
 autoClicker(100, 1);
 
-
-
-//stuff
-
-function reload()
-{
-    var buttonOptions = document.getElementById("prefsButton");
-    var optionsMenu = document.getElementsByClassName("option");
-    var lenMenu = optionsMenu.length;
-    buttonOptions.click();
-    Game.WriteSave();
-    Game.ExportSave();
-    var saveData = document.getElementById("textareaPrompt").innerHTML;
-    Game.ClosePrompt();
-    Game.ImportSave();
-    document.getElementById("textareaPrompt").innerHTML = saveData;
-    document.getElementById("promptOption0").click();
-}
-
-function reloadForBadCookie()
-{
-    if (Game.frenzyPower == 0.5)
-    {
-        reload();
-    }
-}
-
-reloadForBadCookie();
-
-//break
-
-javascript:
-function reloadWrinlkle()
-{
-    var buttonOptions = document.getElementById("prefsButton");
-    var optionsMenu = document.getElementsByClassName("option");
-    var lenMenu = optionsMenu.length;
-    buttonOptions.click();
-    Game.ImportSave();
-    document.getElementById("textareaPrompt").innerHTML = "Mnx8MTQ2NTk1NDg5ODk3MjsxNDU1NzY3NzYyNTcxOzE0NjYwMjcwMjM3MzE7U3B1bmdyYWR8w6TCssKoBHw1LjQ4NTIyMzczODc1MDE4MmUrMzQ7MS44OTAwMjQxMDcxNDQ1MTZlKzM3OzE0NjA0NDE2MDA7NTUwNDY7MS44ODk4MTE1MjA0MzYxMDAyZSszNzs4NDg0OzA7Mzs3LjI3NTkxMTEyMDgzNzk0ZSszNjsxOzA7MDswOy0xOzIxOzczMTs0LjM2ODIzMjE3MzI5OTYyNmUrMzI7MTsxNDsyOzI0Nzc2MTg7NTtoYWxsb3dlZW47NS4xOTgxMjc0ODgwMjE0MWUrMzM7MTI7MTkzNzc0MTI5OzE3NjU2NDM4NzsxNzIwODU1NjswOzA7MTg5OzMyMTszMjI7MzIwOzIyNTsyMTsxNTsxMDswOzA7fDU0Niw4OTYsMS4yNTY2MzcwMjk3Nzk1NjNlKzI3LDA7NTU1LDkwNSw4LjIxMTcxNTQ1MzgyMjU0NWUrMjQsMDs1MjUsODc1LDEuMTI4ODIzMjg2ODE5MDczMWUrMjYsMDs0ODksODM5LDMuNjM2MjY4NTgzMzk0OTE1ZSsyNiwwOzQ5Miw4NDIsMS4yOTU5MjMzMjYxMzAzNTQ1ZSsyNywwOzQ3MSw4MjEsMy4zMzY1MDU2OTg3NTc1NDk2ZSsyNiwwOzQ2Miw4MTIsMS40NDMyMTAxOTQxNjY0NDExZSsyNywwOzQ0MSw3OTEsNS44ODQ0NTk0Njk0NzU3MDI0ZSsyNiwwOzQxNSw3NjUsMi4zMzQyNTE4ODg4NzUxNDQzZSsyNiwwOzM5Miw3NDIsMS4xNDE3MzQ2NjIwMDUyNjUxZSsyNywwOzM4NSw3MzUsNi44NzQ4NjA5MjAwMTgzMzZlKzI3LDA7MzcxLDcyMSw0LjA2NTk4MzIwNjc3NzIyMmUrMjgsMDszNDUsNjk1LDEuODYwMTAxMjEwNjUzNTU3ZSsyOCwwOzM0MSw2OTMsMS4wNzk0OTk0ODk1Nzk5NzE5ZSsyOSwwO3zDp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8Kgwo/Dp8KzwrzDpMKMwr/DpMK/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wrPDp8K/wr/Dp8KAwoDDpMKwwoPDpMKzwr/Dp8K/wr/Dp8K/wrzDp8K/wr/Dp8K/wr/DpsK6wo/Dp8K/wr/Dp8K/wr/Dp8K/wrLDp8K/wr/Dp8K/wr/Dp8K+wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K4wq/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wq/Dp8K/wr/Dp8K/wr/Dp8K/wr/Dp8K/wr9/fMOnwr/Cv8Onwr/Cv8Onwr/Cv8Onwr/Cv8Onwr/Cv8Olwr/Cv8Onwr/CvMOkwr/Cv8Onwr/Cv8Onwr/Cv8Onwr/Cv8OnwrvCv8Onwr/Cv8Onwr/Cv8Onwr/Cs8Onwr/Cv8Onwr/Cv8Onwr/Cv8Ogwr/Cvg%3D%3D%21END%21";
-    document.getElementById("promptOption0").click();
-    document.getElementById("statsButton").click();    
-}
+*/
