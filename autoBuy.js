5632adb9a6c3901cef5965cf93f0268b044f3f33
komradspungrad <spangrpl@gmail.com>
2016-07-07 18:01:47 -0400

    typo

--- autoBuy.js
+++ autoBuy.js
@@ -26,7 +26,7 @@ var legacy = gBid("legacyButton");
 var prestigeNow = 25;
 var trackBuy = 0;
 var trackBuy10 = 0;
-var trackbuy100 = 0;
+var trackBuy100 = 0;
 
 //Interval manager
 function intervalSet(on) {
